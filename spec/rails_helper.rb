b61a256ce798aa711e66c881dfa23bf08ac0c68c
i544c <leo@i544c.me>
2017-09-23 17:19:37 +0900

    dashboardのテストを追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+require_relative "support/controller_macros"
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
@@ -57,4 +59,5 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  config.include ControllerMacros, type: :controller
 end
