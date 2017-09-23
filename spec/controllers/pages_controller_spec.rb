19eba3fc1cee48b21651852ebeb5eea86464f8a1
i544c <leo@i544c.me>
2017-09-23 15:10:54 +0900

    dashboardのテストを追加

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,8 +2,20 @@ require "rails_helper"
 
 RSpec.describe PagesController, type: :controller do
   describe "GET#index" do
+    subject { get :index }
+
     it "レスポンスが200" do
-      expect(response.status).to eq 200
+      is_expected.to have_http_status(200)
+    end
+  end
+
+  describe "GET#dashboard" do
+    subject { get :dashboard }
+
+    context "未ログインの時" do
+      it "ログイン画面に遷移する" do
+        is_expected.to redirect_to new_user_session_path
+      end
     end
   end
 end
