e61d168acf4cab4d57f57e7541e8b1a33a44ac84
i544c <leo@i544c.me>
2017-10-23 12:48:19 +0900

    RailsAdminを追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@
 #  unconfirmed_email      :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  is_admin               :boolean          default(FALSE)
 #
 
 class User < ApplicationRecord
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019170838) do
+ActiveRecord::Schema.define(version: 20171023034002) do
 
   create_table "relations", force: :cascade do |t|
     t.integer "follower_id"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20171019170838) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
