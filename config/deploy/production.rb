eead074a4c2defdc5ffd9dfe0251955406d7a67b
i544c <leo@i544c.me>
2018-01-31 21:14:56 +0900

    Capistranoの設定を修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,9 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "schwul.babu.ml", user: "isaac", roles: %w{app db web}
+server "schwul.babu.ml", user: "isaac", roles: %w{app db web}, ssh_options: {
+  forward_agent: true,
+}
 
 
 
