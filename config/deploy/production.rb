af66f18217106d8b46e2a729bea8649d9a31b4aa
i544c <leo@i544c.me>
2018-01-31 17:59:51 +0900

    Capistranoを設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server "schwul.babu.ml", user: "isaac", roles: %w{app db web}
+
 
 
 # role-based syntax
