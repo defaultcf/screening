c6c2ed710491c4930f5353512f24960ff97f46de
i544c <leo@i544c.me>
2019-07-13 16:49:34 +0900

    Fix for test

--- app.Dockerfile
+++ app.Dockerfile
@@ -1,6 +1,6 @@
 FROM ruby:2.5.5-alpine3.9
 RUN echo 'http://dl-cdn.alpinelinux.org/alpine/v3.5/main' >> /etc/apk/repositories
-RUN apk update && apk add --no-cache alpine-sdk ruby-dev postgresql-dev nodejs npm tzdata imagemagick=6.9.6.8-r1 imagemagick-dev=6.9.6.8-r1 openssh python linux-headers xz-libs
+RUN apk update && apk add --no-cache alpine-sdk ruby-dev postgresql-dev nodejs npm tzdata imagemagick=6.9.6.8-r1 imagemagick-dev=6.9.6.8-r1 openssh python linux-headers xz-libs libressl2.7-libssl
 RUN gem install bundler:2.0.1 && npm i -g yarn
 RUN ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 WORKDIR /app
--- spec/controllers/screenings_controller_spec.rb
+++ spec/controllers/screenings_controller_spec.rb
@@ -40,28 +40,28 @@ RSpec.describe ScreeningsController, type: :controller do
   describe "GET #index" do
     it "returns a success response" do
       get :index
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
   describe "GET #show" do
     it "returns a success response" do
       get :show, params: { id: screening.id }
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
   describe "GET #new" do
     it "returns a success response" do
       get :new
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
   describe "GET #edit" do
     it "returns a success response" do
       get :edit, params: { id: screening.id }
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
