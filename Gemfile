db59274fa2bc487647583bc20f8d81c10c8f0393
i544c <leo@i544c.me>
2019-07-13 18:16:27 +0900

    rails app:update

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem "jbuilder", "~> 2.5"
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Reduces boot times through caching; required in config/boot.rb
+gem "bootsnap", ">= 1.1.0", require: false
+
 # Rack HTTP Server
 gem "unicorn", "~> 5.4"
 # プロセス管理
--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,12 +8,17 @@ Bundler.require(*Rails.groups)
 
 module Schwul
   class Application < Rails::Application
-    config.load_defaults 5.1
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
     config.time_zone = "Asia/Tokyo"
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
